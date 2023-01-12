4b0212a13f5b02988f3d7ed4ec4cc3a78e31450a
HamzaI2000 <90645281+HamzaI2000@users.noreply.github.com>
2023-01-12 18:38:11 -0500

    Add files via upload

---
+++ intro_to_python.py
@@ -0,0 +1,17 @@
+import numpy as np
+ 
+arr = np.array([[1, 0, 0,],
+                [0, 1, 0,],
+                [0, 0, 1,]])
+                
+arr2 = np.array([[1, 3, 3],
+                 [3, 1, 3],
+                 [3, 3, 1]])
+                 
+arr3 = np.array([[1, 3],
+                 [3, 1],
+                 [3, 3]])
+print(arr) 
+print(arr2)
+print(arr3)
+ 
